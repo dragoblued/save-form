4842c113a3e75ac2700a165feebbef65fb1a58ef
Natalia <qwemee@gmail.com>
2022-02-09 07:47:53 +0700

    [main] Adding validation

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -6,14 +6,25 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\FeedbackMail;
 use App\Models\Feedback;
+use Illuminate\Support\Facades\Validator;
 
 
 class FeedbackController extends Controller
 {
     public function send(Request $request) {
-        $feedback = Feedback::create($request->all());
-		Mail::to("qwemee@gmail.com")->send(new FeedbackMail($feedback));
-		// Mail::to("ab@acruxcs.com")->send(new FeedbackMail($feedback));
-		// Mail::to("info@acruxcs.com")->send(new FeedbackMail($feedback));
+        $validator =  Validator::make($request->all(), [
+            'email' => 'required',
+            'phone' => 'required',
+            'name' => 'required',
+        ]);
+        if(!$validator->fails()) {
+            $feedback = Feedback::create($request->all());
+            Mail::to("qwemee@gmail.com")->send(new FeedbackMail($feedback));
+            // Mail::to("ab@acruxcs.com")->send(new FeedbackMail($feedback));
+            // Mail::to("info@acruxcs.com")->send(new FeedbackMail($feedback));
+            return response()->json('success');
+        } else {
+            return response()->json('There are empty fields: email, phone or name');
+        }
     }
 }
